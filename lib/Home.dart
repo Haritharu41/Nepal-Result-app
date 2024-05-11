dcde2d18f19e4d49f872caa38ea346a01cf42517
Hari Prasad tharu <harip4077@gmail.com>
2024-05-11 06:22:22 +0545

    Result nepal app using url launcher

---
+++ lib/Home.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:result_nepal/Result_screen.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.tealAccent,
+        centerTitle: true,
+        title: Text("Result Nepal"),
+      ),
+      body: GridView.count(
+        crossAxisSpacing:2,
+        mainAxisSpacing: 2,
+        crossAxisCount: 2,
+        children: [
+          containerBox(
+              "CTEVT Result", "https://itms.ctevt.org.np:5580/check_results"),
+          containerBox("NEB Result", "https://neb.ntc.net.np/results.php"),
+          containerBox("TU Result", "https://tuexam.edu.np/otuexam/tuexam/"),
+          containerBox("KU Result", "https://ku.edu.np/news-app?tags=Result&search_site_name= "),
+        ],
+      ),
+    );
+  }
+}
+
+class containerBox extends StatelessWidget {
+  String title;
+  String url;
+  containerBox(this.title, this.url);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+
+        child: Container(
+          
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+              color: Colors.teal, borderRadius: BorderRadius.circular(30)),
+          child: Center(
+              child: Text(
+            title,
+            style: TextStyle(color: Colors.white, fontSize: 25),
+          )),
+        ),
+        onTap: () {
+        launchUrlString(url);
+        },
+        
+      ),
+    );
+  }
+}
---
+++ lib/Result_screen.dart
@@ -0,0 +1,31 @@
+
+
+// import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+//  class resultScreen extends StatefulWidget {
+//     String url;
+//     resultScreen(this.url);
+     
+  
+
+//   @override
+//   State<resultScreen> createState() => _resultScreenState();
+// }
+
+// class _resultScreenState extends State<resultScreen> {
+//   final controller=WebViewController()
+//   ..setJavaScriptMode(JavaScriptMode.disabled)
+//   ..loadRequest(Uri.parse(url));
+
+  
+  
+ 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text(""),)
+//     ,
+//     body: WebViewWidget(controller: controller),
+//     );
+//   }
+// }
